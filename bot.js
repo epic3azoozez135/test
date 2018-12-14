05f4831483fe994d70fe8618e9db830eee8cd39b
Azoooooooooz <44898336+epic3azoozez135@users.noreply.github.com>
2018-12-15 00:26:13 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,28 @@
- 
+ client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`الشيء الي تبيه`,)
+  console.log('')
+  console.log('')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log(`[Start] ${new Date()}`);
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════════════════════════════]╗');
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
+  console.log('')
+  console.log('Informations :')
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════]╗')
+  console.log(' Bot Is Online')
+  console.log('╚[════════════]╝')
+  console.log('')
+  console.log('')
+});
   client.on('message', msg => {//msg
     if (msg.content === `${prefix}colors`) {
       msg.channel.send({file : "https://cdn.discordapp.com/attachments/501774006966419458/501774646467887105/colors.png"})
@@ -2368,3 +2392,4 @@ if (message.content.startsWith(`${prefix}deletecolors`)) {
 
 });
 })
+client.login(process.env.BOT_TOKEN);
